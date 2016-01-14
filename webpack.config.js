53dac6871345dded4129ca903f19443f38ccfd7d
Krzysztof Syrytczyk <ksyrytczyk@cyfrowypolsat.pl>
2016-01-14 20:27:01 +0100

    v1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: './index.js',
+
+    output: {
+        library: 'InspectumMobile',
+        libraryTarget: 'umd',
+
+        filename: 'InspectumMobile.js',
+        path: './dist/'
+    }
+};
